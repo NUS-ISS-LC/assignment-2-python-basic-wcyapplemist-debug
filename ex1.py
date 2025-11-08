09c6940861b2143665c16c6a0953efd011159c81
Wang Chenyu <wcyapplemist@gmail.com>
2025-11-08 07:51:14 +0000

    Completed Assignment for Ex1

--- ex1.py
+++ ex1.py
@@ -1,3 +1,11 @@
 def find(s, n):
 # write your implementation here
+    for i in range(len(s)):
+        if s[i]+s[i+1] == n:
+            return [i, i+1]
+            break
     return None
+
+print(find([2,7,11,15], 9))
+print(find([3,2,4], 6))
+print(find([3,3], 6))
